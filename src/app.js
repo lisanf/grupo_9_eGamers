3bb7f0c9ce9f456cf3ca4f3e6ff51b0d69a56ede
ploriente <ploriente@yahoo.com>
2022-12-18 23:44:57 -0300

    codeada los accesos a las vistas en app.js

--- src/app.js
+++ src/app.js
@@ -3,3 +3,30 @@ const path = require('path');
 
 let app = express();
 
+app.use(express.static("public"));
+
+// Ponemos a escuchar el servidor
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Servidor corriendo en http://localhost:" + port)
+});
+
+// Definimos las rutas a los distintos pedidos que nuestro sitio sabe responder
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+})
+
+app.get("/productDetail", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
+})
+
+app.get("/productCart", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
+})
